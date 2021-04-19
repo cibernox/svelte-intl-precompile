193c5d95c4afe25461751e26c81a3207b7bf4bc6
Miguel Camba <miguel.camba@gmail.com>
2021-04-20 00:52:02 +0200

    v0.1.1

--- sveltekit-plugin.js
+++ sveltekit-plugin.js
@@ -4,16 +4,21 @@ const path = require('path');
 
 const intlPrecompiler = buildPlugin('svelte-intl-precompile');
 
-module.exports = function svelteIntlPrecompile(localesRoot) {  
+function transformCode(code) {
+	return babel.transform(code, { plugins: [intlPrecompiler] }).code;
+}
+
+function svelteIntlPrecompile(localesRoot) {  
 	return {
 	  	name: 'svelte-intl-precompile', // required, will show up in warnings and errors
 		transform(code, id) {	
 			if (id.includes(path.resolve(localesRoot))) {
-				return babel.transform(code, {
-					plugins: [intlPrecompiler],
-
-				}).code;
+				return transformCode(code);
 			}
 		}
 	}
 }
+
+svelteIntlPrecompile.transformCode = transformCode;
+
+module.exports = svelteIntlPrecompile;
