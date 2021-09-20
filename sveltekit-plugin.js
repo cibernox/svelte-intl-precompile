f94075ea19a3ce85b214f6eb40832bf9c8c1da4b
Vladimir Kleshko <uside.node@gmail.com>
2021-09-20 17:47:57 +0300

    Add support for .json files

--- sveltekit-plugin.js
+++ sveltekit-plugin.js
@@ -1,18 +1,51 @@
 import babel from '@babel/core';
 import buildPlugin from 'babel-plugin-precompile-intl';
 import path from 'path';
+import fs from 'fs';
 
 const intlPrecompiler = buildPlugin('svelte-intl-precompile');
+
+function detectLanguageCode (filepath) {
+	return path.basename(filepath).split('.')[0]
+}
 
 function transformCode(code) {
 	return babel.transform(code, { plugins: [intlPrecompiler] }).code;
 }
 
-function svelteIntlPrecompile(localesRoot) {  
+function svelteIntlPrecompile(localesRoot, prefix = '$locales') {
+	const resolvedPath = path.resolve(localesRoot)
+
 	return {
-	  	name: 'svelte-intl-precompile', // required, will show up in warnings and errors
-		transform(code, id) {	
-			if (id.includes(path.resolve(localesRoot))) {
+		name: 'svelte-intl-precompile', // required, will show up in warnings and errors
+		configureServer(server) {
+			const { ws, watcher, moduleGraph } = server
+
+			watcher.on('change', async(file) => {
+				if(file.includes(resolvedPath)){
+					const module = moduleGraph.getModuleById(`${prefix}/${detectLanguageCode(file)}.ts`);
+					if (module) {
+						moduleGraph.invalidateModule(module);
+					}
+					ws.send({ type: 'full-reload', path: '*' })
+				}
+			})
+		},
+		resolveId(id) {
+			if (id.startsWith(prefix)) {
+				return id;
+			}
+		},
+		load(id) {
+			if (id.startsWith(prefix)) {
+				const code = fs.readFileSync(path.join(localesRoot, `${detectLanguageCode(id)}.json`), {
+					encoding: 'utf-8'
+				});
+				return transformCode(`export default ${code}`);
+			}
+		},
+		transform(code, id) {
+			if (id.includes(resolvedPath)) {
 				return transformCode(code);
 			}
 		}
