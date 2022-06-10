4d52e5fe6a5c701fc1f9fb9578ac1fd521e0c34f
Miguel Camba <miguel.camba@gmail.com>
2022-06-10 22:07:08 +0200

    Refine test checks

--- tests/sveltekit-plugin.test.ts
+++ tests/sveltekit-plugin.test.ts
@@ -1,8 +1,38 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import svelteIntlPrecompile from '../sveltekit-plugin';
 
-describe('suite', () => {
-  it('serial test', async () => { 
-    expect(svelteIntlPrecompile).toBeTruthy();
-  })
+vi.mock('path', () => ({
+  resolve(path) {
+    return 'fakeroot/' + path;
+  },
+  extname(filename) {
+    return filename.split('.')[1];
+  }
+}));
+
+vi.mock('fs/promises', () => ({
+  readdir() {
+    return Promise.resolve().then(() => ['en-US.json', 'en.json', 'es.json'])
+  },
+}));
+
+describe('imports', () => {
+  it('$locales returns a module with all the available locales', async () => { 
+    const plugin = svelteIntlPrecompile('locales');
+    const content = await plugin.load('$locales');
+    expect(content).toBe(singleLineString`
+    import { register } from 'svelte-intl-precompile'
+    export function registerAll() {
+    }
+    export const availableLocales = []`)
+  });
 });
+
+function singleLineString([str]: TemplateStringsArray) {
+  let lines: string[] = str.split('\n');
+  if (lines[0] === '') {
+    lines = lines.splice(1);
+  }
+  let firstLineSpaces = lines[0].search(/\S|$/);
+  return lines.map(l => l.substring(firstLineSpaces)).join('\n');
+}
